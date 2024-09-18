8e7df7132dd4b259c8992a18437821ed8871d768
Piero Dev <pierodev21@gmail.com>
2024-09-18 01:55:55 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+<div align="center">
+    <!-- <a href="https://todo-react-app-iota-seven.vercel.app/"> -->
+    <img src="./screenshot.jpeg" /> 
+    </a>
+  <h3>
+    <strong>Forkify Recipe App</strong>
+  </h3>
+  <!-- <a href="https://todo-react-app-iota-seven.vercel.app/">
+        ✨ Website ✨
+    </a> -->
+</div>
+
+## 🛠️ Stack
+![Typescript](https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white)
+![SASS](https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white)
+
+**Description:**
+Forkify is a recipe app that allows users to search and discover new recipes. The app uses the Forkify API to fetch recipe data and provides a user-friendly interface to browse and view recipes.
+
+**Features:**
+
+* Search for recipes by keyword or ingredient
+* Browse through a list of popular recipes
+* View detailed recipe information, including ingredients and cooking instructions
+* Error handling for API requests and recipe data
+
+**Technologies Used:**
+
+* TypeScript for front-end development
+* Parcel for bundling and serving the application
+* Forkify API for recipe data
+
+**Getting Started:**
+1. Install pnpm using npm install -g pnpm or yarn global add pnpm
+2. Clone the repository using `git clone https://github.com/your-username/your-repo-name.git`
+3. Install dependencies using `pnpm install`
+4. Start the development server using `pnpm dev`
+5. Open the app in your browser at `http://localhost:1234`
+
---
+++ src/script/controller.ts
@@ -0,0 +1,162 @@
+// import 'core-js/stable';
+// import 'regenerator-runtime/runtime';
+const icons = new URL('../img/icons.svg', import.meta.url);
+const recipeContainer = document.querySelector('.recipe') as HTMLElement;
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+function renderSpiner(parent) {
+  const markup = `<div class="spinner">
+          <svg>
+            <use href="${icons}#icon-loader"></use>
+          </svg>
+        </div>`;
+
+  parent.innerHTML = '';
+  parent.insertAdjacentHTML('afterbegin', markup);
+}
+const showRecipes = async function () {
+  const id  = window.location.hash.slice(1);
+  if(!id) return;
+  try {
+    //1. Loading recipes
+    renderSpiner(recipeContainer);
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(`${data.message} (${res.status})`);
+    }
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      imageUrl: recipe.image_url,
+      publisher: recipe.publisher,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      title: recipe.title,
+      ingredients: recipe.ingredients,
+      sourceUrl: recipe.source_url,
+    };
+
+    const markup = `
+    <figure class="recipe__fig">
+          <img src="${recipe.image}" alt="${
+      recipe.title
+    }" class="recipe__img" />
+          <h1 class="recipe__title">
+            <span>${recipe.title}</span>
+          </h1>
+        </figure>
+
+        <div class="recipe__details">
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="${icons}#icon-clock"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--minutes">${
+              recipe.cookingTime
+            }</span>
+            <span class="recipe__info-text">minutes</span>
+          </div>
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="${icons}#icon-users"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--people">${
+              recipe.servings
+            }</span>
+            <span class="recipe__info-text">servings</span>
+
+            <div class="recipe__info-buttons">
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="${icons}#icon-minus-circle"></use>
+                </svg>
+              </button>
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="${icons}#icon-plus-circle"></use>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div class="recipe__user-generated">
+            <svg>
+              <use href="${icons}#icon-user"></use>
+            </svg>
+          </div>
+          <button class="btn--round">
+            <svg class="">
+              <use href="${icons}#icon-bookmark-fill"></use>
+            </svg>
+          </button>
+        </div>
+
+        <div class="recipe__ingredients">
+          <h2 class="heading--2">Recipe ingredients</h2>
+          <ul class="recipe__ingredient-list">
+            ${recipe.ingredients
+              .map(
+                ing =>
+                  `<li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="${icons}#icon-check"></use>
+              </svg>
+              <div class="recipe__quantity">${ing.quantity || 0}</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">${ing.unit}</span>
+                ${ing.description}
+              </div>
+            </li>`
+              )
+              .join('')}            
+          </ul>
+        </div>
+
+        <div class="recipe__directions">
+          <h2 class="heading--2">How to cook it</h2>
+          <p class="recipe__directions-text">
+            This recipe was carefully designed and tested by
+            <span class="recipe__publisher">${
+              recipe.publisher
+            }</span>. Please check out
+            directions at their website.
+          </p>
+          <a
+            class="btn--small recipe__btn"
+            href="${recipe.sourceUrl}"
+            target="_blank"
+          >
+            <span>Directions</span>
+            <svg class="search__icon">
+              <use href="./img/icons.svg#icon-arrow-right"></use>
+            </svg>
+          </a>
+        </div>
+    `;
+    recipeContainer.innerHTML = '';
+    recipeContainer?.insertAdjacentHTML('afterbegin', markup);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+showRecipes();
+
+
+window.addEventListener('hashchange',showRecipes);
+// window.addEventListener('load',showRecipes);
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
